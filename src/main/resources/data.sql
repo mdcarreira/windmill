4e0e1c37e25f4af9ea91521d02b1a07e06d17f11
mdcarreira <mdcarreira@icloud.com>
2020-03-29 19:23:25 +0100

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into user(id,username,encryted_password,admin) values (10001, 'admin', '$2a$10$/dX22igCYeVo6l3Bit1cOOuQkqS/uwT9bB10RktQATmXg.M8quw26', true);
+insert into user(id,username,encryted_password,admin) values (10002, 'user', '$2a$10$aWW/H7d55T1lRTNhF3gs3uqksFVcOlbC6aE2oWq2GsT.efLp.JOBy', false);
